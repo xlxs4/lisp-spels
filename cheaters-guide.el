9cabda7c5e346a446a266fedcba881750829596b
xlxs4 <orousoultzoglou@gmail.com>
2023-03-01 23:25:20 +0200

    Add cheater's guide

---
+++ cheaters-guide.el
@@ -0,0 +1,87 @@
+;; ELISP> (look)
+;; (you are in the living room of a wizards house -
+;;      there is a wizard snoring loudly on the couch -
+;;      there is a door going west from here -
+;;      there is a stairway going upstairs from here -
+;;      you see a whiskey-bottle on the floor -
+;;      you see a bucket on the floor -)
+
+;; ELISP> (pickup bucket)
+;; (you are now carrying the bucket)
+
+;; ELISP> (inventory)
+;; (bucket)
+
+;; ELISP> (walk upstairs)
+;; (you are in the attic of the wizards house -
+;;      there is a giant welding torch in the corner -
+;;      there is a stairway going downstairs from here -)
+
+;; ELISP> (walk east)
+;; (you cannot go that way -)
+
+;; ELISP> (walk downstairs)
+;; (you are in the living room of a wizards house -
+;;      there is a wizard snoring loudly on the couch -
+;;      there is a door going west from here -
+;;      there is a stairway going upstairs from here -
+;;      you see a whiskey-bottle on the floor -)
+
+;; ELISP> (walk west)
+;; (you are in a beautiful garden -
+;;      there is a well in front of you -
+;;      there is a door going east from here -
+;;      you see a frog on the floor -
+;;      you see a chain on the floor -)
+
+;; ELISP> (dunk bucket well)
+;; (the water level is too low to reach -)
+
+;; ELISP> (pickup chain)
+;; (you are now carrying the chain)
+
+;; ELISP> (walk east)
+;; (you are in the living room of a wizards house -
+;;      there is a wizard snoring loudly on the couch -
+;;      there is a door going west from here -
+;;      there is a stairway going upstairs from here -
+;;      you see a whiskey-bottle on the floor -)
+
+;; ELISP> (splash bucket wizard)
+;; (the bucket has nothing in it -)
+
+;; ELISP> (walk upstairs)
+;; (you are in the attic of the wizards house -
+;;      there is a giant welding torch in the corner -
+;;      there is a stairway going downstairs from here -)
+
+;; ELISP> (weld chain bucket)
+;; (the chain is now securely welded to the bucket -)
+
+;; ELISP> (walk downstairs)
+;; (you are in the living room of a wizards house -
+;;      there is a wizard snoring loudly on the couch -
+;;      there is a door going west from here -
+;;      there is a stairway going upstairs from here -
+;;      you see a whiskey-bottle on the floor -)
+
+;; ELISP> (walk west)
+;; (you are in a beautiful garden -
+;;      there is a well in front of you -
+;;      there is a door going east from here -
+;;      you see a frog on the floor -)
+
+;; ELISP> (dunk bucket well)
+;; (the bucket is now full of water)
+
+;; ELISP> (walk east)
+;; (you are in the living room of a wizards house -
+;;      there is a wizard snoring loudly on the couch -
+;;      there is a door going west from here -
+;;      there is a stairway going upstairs from here -
+;;      you see a whiskey-bottle on the floor -)
+
+;; ELISP> (splash bucket wizard)
+;; (the wizard awakens from his slumber and greets you warmly -
+;;      he hands you the magic low-carb donut -
+;;      you win! the end -)
